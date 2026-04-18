58fa09377fc03232b1e81a55df08892f49416228
Miłosz Kluzik <djpromilly@gmail.com>
2026-04-18 23:03:31 +0200

    feat: dodano content-type: IntegrationSettings oraz Projects

---
+++ backend/src/api/integration-settings/services/integration-settings.ts
@@ -0,0 +1,7 @@
+/**
+ * integration-settings service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::integration-settings.integration-settings');
