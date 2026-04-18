1814992d4cf741a51b027657474917e163e09eaa
Marek Hajduk <marekhajduk.pl@gmail.com>
2026-04-18 20:24:40 +0200

    feat: Dodano dashboard

---
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+ 
+import './assets/index.scss';
+ 
+const Dashboard: React.FC = () => {
+ 
+  return (
+    <div className="page-container dashboard-page">
+      <h1>Panel główny</h1>
+    </div>
+  );
+};
+ 
+export default Dashboard;
