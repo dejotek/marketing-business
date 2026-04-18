e14b97e76b193d470cbdec17ca6322ab3c5a4491
Miłosz Kluzik <djpromilly@gmail.com>
2026-04-18 22:58:18 +0200

    feat: dodano content-type: CoursesProgress

---
+++ backend/src/api/course-progress/controllers/course-progress.ts
@@ -0,0 +1,7 @@
+/**
+ * course-progress controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::course-progress.course-progress');
---
+++ backend/src/api/course-progress/routes/course-progress.ts
@@ -0,0 +1,7 @@
+/**
+ * course-progress router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::course-progress.course-progress');
