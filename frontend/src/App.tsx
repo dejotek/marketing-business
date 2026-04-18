f1e112fce636c965cbd264f7d5a9bea56e5b6c9c
Marek Hajduk <marekhajduk.pl@gmail.com>
2026-04-18 20:30:02 +0200

    feat: Dodano routing i dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+ 
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+ 
+import Login from './components/login';
+import Register from './components/register';
+import Dashboard from './components/dashboard';
+ 
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </Router>
+);
+ 
 export default App;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
  
-const container = document.getElementById('root')
-if (container) {
-  const root = createRoot(container)
-  root.render(<App />)
-}
+import reportWebVitals from './reportWebVitals';
+ 
+import App from './App';
+ 
+import './index.css';
+ 
+ 
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ 
+reportWebVitals();
