80ec4e47398b71293d0ac7f4636522becf397282
Marek Hajduk <marekhajduk.pl@gmail.com>
2026-04-19 12:00:01 +0200

    feat: dodano layout.tsx

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+ 
+import { Outlet } from 'react-router-dom';
+import Sidebar from './sidebar';
+ 
+// import './assets/layout.scss';
+ 
+const ProtectedLayout: React.FC = () => {
+ 
+  return (
+    <div className="app-wrapper">
+      <Sidebar />
+      <main className="main-content">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+ 
+export default ProtectedLayout;
